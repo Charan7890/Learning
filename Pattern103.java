c06905b15b09c0f9f23cddd78394cb19a1f70f86
Charan Naik <81156598+Charan7890@users.noreply.github.com>
2022-10-26 13:42:54 +0530

    Create Pattern103.java

---
+++ Pattern103.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		/*
+		    col(n)
+		  0 1 2 3 4
+		0 * * * * *
+		1 * * * *
+ row(n) 2 * * *
+		3 * *
+		4 *
+		
+		*/
+		int count = 1; // 11 ---> 1 changing
+		for(int i=0;i<n;i++)
+		{
+		for(int j = 0;j<n-i;j++)
+		{
+		  //  if(count>=1 && count<=9){
+		  //    System.out.print(count+" ");  
+		  //    count++;
+		  //  }
+		  //  else{
+		       System.out.print(count+" ");
+		       count++; 
+		    //}
+		    
+		}
+		System.out.println();
+		count = 1;
+		}
+	}
+}
